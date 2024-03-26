63dff3a4da3781f858244005d51af1bfe93ea076
sumanthyeruva <yeruva.sumanth07@gmail.com>
2024-03-26 15:41:31 -0500

    Imported all required directories by Sumanth Yeruva(S567115)

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Agri_Shop"
+include(":app")
+ 
